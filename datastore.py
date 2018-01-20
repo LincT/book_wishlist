f1bc794c91325f55cb765b3279b6ddccf13f5b24
Linc <test@testserver.com>
2018-01-19 23:47:41 -0600

    full merge, master should now be current with all branches and all conflicts resolved

--- datastore.py
+++ datastore.py
@@ -3,6 +3,7 @@
 from book import Book
 from fileIO import FileIO as fileIO
 from datetime import date
+import json
 
 DATA_DIR = 'data'
 BOOKS_FILE_NAME = str(fileIO.pathJoin(DATA_DIR, 'wishlist.txt'))
@@ -136,9 +137,24 @@ def make_book_list(string_from_file):
     books_str = string_from_file.split('\n')
 
     for book_str in books_str:
-        data = book_str.split(separator)
-        book = Book(data[0], data[1], data[2] == 'True', int(data[3]))
-        book_list.append(book)
+        if book_str.find(separator) > -1:  # backwards compatibility
+            data = book_str.split(separator)
+            title = data[0]
+            author = data[1]
+            read = data[2] == 'True'
+            book_id = int(data[3])
+            book = Book(title,author,read,book_id)
+            book_list.append(book)
+        else:
+            if len(str(book_str).strip())>=1:
+                data = json.loads(book_str)
+                for entry in data:
+                    title = data[entry]['title']
+                    author = data[entry]['author']
+                    read = data[entry]['read'] == 'True'
+                    book_id = int(str(entry))
+                    book = Book(title,author,read,book_id)
+                    book_list.append(book)
 
 
 def make_output_data():
@@ -149,8 +165,16 @@ def make_output_data():
     output_data = []
 
     for book in book_list:
-        output = [book.title, book.author, str(book.read), str(book.id)]
-        output_str = separator.join(output)
+        # output = [book.title, book.author, str(book.read), str(book.id)]
+        output = \
+            {
+                str(book.id): {
+                    'title':book.title,
+                    'author':book.author,
+                    'read':str(book.read)
+                }
+            }
+        output_str = json.dumps(output)
         output_data.append(output_str)
 
     all_books_string = '\n'.join(output_data)
